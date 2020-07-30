9c1f4ba0788d068a644610d97075d2f7737def63
Clementine Urquizar <clementine@meilisearch.com>
2020-07-30 15:34:49 +0200

    Update script

--- scripts/check-readme.sh
+++ scripts/check-readme.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+# Checking that cargo is installed
+if ! command -v cargo &> /dev/null
+then
+    echo 'You must install cargo to make this script working.'
+    echo 'See https://doc.rust-lang.org/cargo/getting-started/installation.html'
+    exit
+fi
+
 cargo install cargo-readme
 
 curent_readme="README.md"
-generated_readme="$(cargo readme > README.md)"
+generated_readme="README.md_tmp"
+cargo readme > "$generated_readme"
 
-diff "$curent_readme" "$generated_readme"
+diff "$curent_readme" "$generated_readme" &> /dev/null
+if [[ "$?" -eq 0 ]]; then
+    echo "OK"
+    rm -f "$generated_readme"
+    exit 0
+else
+    echo "The current README.md is not up-to-date with the template."
+    echo "Run: sh scripts/update-readme.sh"
+    rm -f "$generated_readme"
+    exit 1
+fi
