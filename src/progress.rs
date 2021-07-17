8077e0b09f0b771f49475ca1b2f61022578ddf7b
soeur <juzi201314@gmail.com>
2021-07-17 19:39:02 +0800

    New update status value

--- src/progress.rs
+++ src/progress.rs
@@ -71,12 +71,12 @@ impl<'a> Progress {
     }
 
     /// Wait until MeiliSearch processes an update, and get its status.
-    /// 
+    ///
     /// `interval` = The frequency at which the server should be polled. Default = 50ms
     /// `timeout` = The maximum time to wait for processing to complete. Default = 5000ms
-    /// 
+    ///
     /// If the waited time exceeds `timeout` then `None` will be returned.
-    /// 
+    ///
     /// # Example
     ///
     /// ```
@@ -89,7 +89,7 @@ impl<'a> Progress {
     /// #    value: String,
     /// #    kind: String,
     /// # }
-    /// # 
+    /// #
     /// # impl document::Document for Document {
     /// #    type UIDType = usize;
     /// #
@@ -101,14 +101,14 @@ impl<'a> Progress {
     /// # futures::executor::block_on(async move {
     /// let client = Client::new("http://localhost:7700", "masterKey");
     /// let movies = client.create_index("movies_wait_for_pending", None).await.unwrap();
-    /// 
+    ///
     /// let progress = movies.add_documents(&[
     ///     Document { id: 0, kind: "title".into(), value: "The Social Network".to_string() },
     ///     Document { id: 1, kind: "title".into(), value: "Harry Potter and the Sorcerer's Stone".to_string() },
     /// ], None).await.unwrap();
-    /// 
+    ///
     /// let status = progress.wait_for_pending_update(None, None).await.unwrap();
-    /// 
+    ///
     /// # client.delete_index("movies_wait_for_pending").await.unwrap();
     /// assert!(matches!(status.unwrap(), UpdateStatus::Processed { .. }));
     /// # });
@@ -133,7 +133,7 @@ impl<'a> Progress {
                         UpdateStatus::Failed { .. } | UpdateStatus::Processed { .. } => {
                             return Some(self.get_status().await);
                         },
-                        UpdateStatus::Enqueued { .. } => {
+                        UpdateStatus::Enqueued { .. } | UpdateStatus::Processing { .. } => {
                             elapsed_time += interval;
                             async_sleep(interval).await;
                         },
@@ -245,6 +245,10 @@ pub enum UpdateStatus {
         #[serde(flatten)]
         content: EnqueuedUpdateResult,
     },
+    Processing {
+        #[serde(flatten)]
+        content: EnqueuedUpdateResult,
+    },
     Failed {
         #[serde(flatten)]
         content: ProcessedUpdateResult,
@@ -268,10 +272,10 @@ mod test {
        value: String,
        kind: String,
     }
-    
+
     impl document::Document for Document {
        type UIDType = usize;
-    
+
        fn get_uid(&self) -> &Self::UIDType {
            &self.id
        }
@@ -296,7 +300,7 @@ mod test {
         let status = progress.wait_for_pending_update(
             Some(Duration::from_millis(1)), Some(Duration::from_millis(6000))
         ).await.unwrap();
-    
+
         client.delete_index("movies_wait_for_pending_args").await.unwrap();
         assert!(matches!(status.unwrap(), UpdateStatus::Processed { .. }));
     }
@@ -320,7 +324,7 @@ mod test {
         let status = progress.wait_for_pending_update(
             Some(Duration::from_millis(1)), Some(Duration::from_nanos(1))
         ).await;
-    
+
         client.delete_index("movies_wait_for_pending_timeout").await.unwrap();
         assert_eq!(status.is_none(), true);
     }
