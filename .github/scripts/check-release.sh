75cd43a0a1e2771fdf51270ebd3fc17b44141f5b
Clémentine Urquizar <clementine@meilisearch.com>
2021-11-18 17:45:58 +0100

    Fix check-release.sh script

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -6,12 +6,12 @@ major=$(echo $current_tag | cut -d '.' -f1 )
 minor=$(echo $current_tag | cut -d '.' -f2 )
 cropped_current_tag="$major.$minor"
 file1='Cargo.toml'
-file2='src/lib.rs'
+file2='README.tpl'
 file3='.code-samples.meilisearch.yaml'
 file4='README.md'
 
 file_tag1=$(grep '^version = ' $file1 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
-file_tag2=$(grep '//! meilisearch-sdk = ' $file2 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
+file_tag2=$(grep 'meilisearch-sdk = ' $file2 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 file_tag3=$(grep 'meilisearch-sdk = ' $file3 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 file_tag4=$(grep 'meilisearch-sdk = ' $file4 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 if [ "$current_tag" != "$file_tag1" ] || [ "$current_tag" != "$file_tag2" ] || [ "$cropped_current_tag" != "$file_tag3" ] || [ "$current_tag" != "$file_tag4" ]; then
