581200a7be8702aec0b9a64e6a4f872969de4ce5
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2022-05-17 20:41:38 +0545

    Add builder pattern to update Keys

--- src/key.rs
+++ src/key.rs
@@ -24,6 +24,180 @@ pub struct Key {
     pub updated_at: OffsetDateTime,
 }
 
+impl Key {
+    /// Update the description of the key.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use meilisearch_sdk::{key::KeyBuilder, key::Action, client::Client};
+    /// # futures::executor::block_on(async move {
+    /// # let client = Client::new("http://localhost:7700", "masterKey");
+    ///
+    ///  let mut key = KeyBuilder::new("My little lovely test key")
+    ///   .with_action(Action::DocumentsAdd)
+    ///   .with_index("*")
+    ///   .create(&client).await.unwrap();
+    ///
+    /// key.with_description("My not so little lovely test key");
+    /// assert_eq!(key.description, "My not so little lovely test key".to_string());
+    /// # client.delete_key(key).await.unwrap();
+    /// # });
+    /// ```
+    pub fn with_description(&mut self, desc: impl AsRef<str>) -> &mut Self {
+        self.description = desc.as_ref().to_string();
+        self
+    }
+
+    /// Add a set of actions the [Key] will be able to execute.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use meilisearch_sdk::{key::KeyBuilder, key::Action, client::Client};
+    /// # futures::executor::block_on(async move {
+    /// # let client = Client::new("http://localhost:7700", "masterKey");
+    ///
+    ///  let mut key = KeyBuilder::new("My little lovely test key")
+    ///   .with_action(Action::DocumentsAdd)
+    ///   .with_index("*")
+    ///   .create(&client).await.unwrap();
+    ///
+    /// key.with_actions([Action::DocumentsGet, Action::DocumentsDelete]);
+    /// # client.delete_key(key).await.unwrap();
+    /// # });
+    /// ```
+    pub fn with_actions(&mut self, actions: impl IntoIterator<Item = Action>) -> &mut Self {
+        self.actions.extend(actions);
+        self
+    }
+
+    /// Add one action the [Key] will be able to execute.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use meilisearch_sdk::{key::KeyBuilder, key::Action, client::Client};
+    /// # futures::executor::block_on(async move {
+    /// # let client = Client::new("http://localhost:7700", "masterKey");
+    ///
+    ///  let mut key = KeyBuilder::new("My little lovely test key")
+    ///   .with_action(Action::DocumentsAdd)
+    ///   .with_index("*")
+    ///   .create(&client).await.unwrap();
+    ///
+    /// key.with_action(Action::DocumentsGet);
+    /// # client.delete_key(key).await.unwrap();
+    /// # });
+    /// ```
+    pub fn with_action(&mut self, action: Action) -> &mut Self {
+        self.actions.push(action);
+        self
+    }
+
+    /// Update the expiration date of the [Key].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use meilisearch_sdk::{key::KeyBuilder, key::Action, client::Client};
+    /// use time::{OffsetDateTime, Duration};
+    /// # futures::executor::block_on(async move {
+    /// # let client = Client::new("http://localhost:7700", "masterKey");
+    ///
+    ///  let mut key = KeyBuilder::new("My little lovely test key")
+    ///   .with_action(Action::DocumentsAdd)
+    ///   .with_index("*")
+    ///   .create(&client).await.unwrap();
+    ///
+    /// // update the epiry date of the key to two weeks from now
+    /// key.with_expires_at(OffsetDateTime::now_utc() + Duration::WEEK * 2);
+    /// # client.delete_key(key).await.unwrap();
+    /// # });
+    /// ```
+    pub fn with_expires_at(&mut self, expires_at: OffsetDateTime) -> &mut Self {
+        self.expires_at = Some(expires_at);
+        self
+    }
+
+    /// Update the indexes the [Key] can manage.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use meilisearch_sdk::{key::KeyBuilder, key::Action, client::Client};
+    /// # futures::executor::block_on(async move {
+    /// # let client = Client::new("http://localhost:7700", "masterKey");
+    ///
+    ///  let mut key = KeyBuilder::new("My little lovely test key")
+    ///   .with_action(Action::DocumentsAdd)
+    ///   .with_index("*")
+    ///   .create(&client).await.unwrap();
+    ///
+    /// key.with_indexes(vec!["test", "movies"]);
+    /// # client.delete_key(key).await.unwrap();
+    /// # });
+    /// ```
+    pub fn with_indexes(
+        &mut self,
+        indexes: impl IntoIterator<Item = impl AsRef<str>>,
+    ) -> &mut Self {
+        self.indexes = indexes
+            .into_iter()
+            .map(|index| index.as_ref().to_string())
+            .collect();
+        self
+    }
+
+    /// Add one index the [Key] can manage.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use meilisearch_sdk::{key::KeyBuilder, key::Action, client::Client};
+    /// # futures::executor::block_on(async move {
+    /// # let client = Client::new("http://localhost:7700", "masterKey");
+    ///
+    ///  let mut key = KeyBuilder::new("My little lovely test key")
+    ///   .with_action(Action::DocumentsAdd)
+    ///   .with_index("*")
+    ///   .create(&client).await.unwrap();
+    ///
+    /// key.with_index("test");
+    /// # client.delete_key(key).await.unwrap();
+    /// # });
+    /// ```
+    pub fn with_index(&mut self, index: impl AsRef<str>) -> &mut Self {
+        self.indexes.push(index.as_ref().to_string());
+        self
+    }
+
+    /// Update the [Key].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use meilisearch_sdk::{key::KeyBuilder, client::Client};
+    /// # futures::executor::block_on(async move {
+    /// let client = Client::new("http://localhost:7700", "masterKey");
+    /// let mut key = KeyBuilder::new("My little lovely test key")
+    ///   .create(&client).await.unwrap();
+    ///
+    /// # assert_eq!(key.description, "My little lovely test key");
+    ///
+    /// key.with_description("My not so little lovely test key");
+    /// let key = key.update(&client).await.unwrap();
+    ///
+    /// # assert_eq!(key.description, "My not so little lovely test key".to_string());
+    ///
+    /// # client.delete_key(key).await.unwrap();
+    /// # });
+    /// ```
+    pub async fn update(&self, client: &Client) -> Result<Key, Error> {
+        client.update_key(self).await
+    }
+}
+
 impl AsRef<str> for Key {
     fn as_ref(&self) -> &str {
         &self.key
