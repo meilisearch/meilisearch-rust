59676192520c1421c0de2bc39b5d5097c3e5e630
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-11-16 15:24:58 +0100

    Add canceled by as a return from task

--- src/tasks.rs
+++ src/tasks.rs
@@ -124,6 +124,7 @@ pub struct SucceededTask {
     pub started_at: OffsetDateTime,
     #[serde(with = "time::serde::rfc3339")]
     pub finished_at: OffsetDateTime,
+    pub canceled_by: Option<String>,
     pub index_uid: Option<String>,
     #[serde(flatten)]
     pub update_type: TaskType,
