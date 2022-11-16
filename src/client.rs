4fde029f85284c7cc43f2c837bf8acd3a4345c35
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-11-16 12:34:12 +0100

    Implement swap indexes

--- src/client.rs
+++ src/client.rs
@@ -21,7 +21,7 @@ pub struct Client {
 
 #[derive(Debug, Clone, Serialize)]
 pub struct SwapIndexes {
-    pub indexes: Option<Vec<String>>,
+    pub indexes: Vec<String>,
 }
 
 impl Client {
@@ -360,13 +360,16 @@ impl Client {
     /// # index.delete().await.unwrap().wait_for_completion(&client, None, None).await.unwrap();
     /// # });
     /// ```
-    pub async fn swap_indexes(&self, indexes: &SwapIndexes) -> Result<TaskInfo, Error> {
+    pub async fn swap_indexes(
+        &self,
+        indexes: impl IntoIterator<Item = &SwapIndexes>,
+    ) -> Result<TaskInfo, Error> {
         request::<(), &SwapIndexes, TaskInfo>(
             &format!("{}/swap-indexes", self.host),
             &self.api_key,
             Method::Post {
                 query: (),
-                body: indexes,
+                body: indexes.into_iter().collect(),
             },
             202,
         )
@@ -937,8 +940,6 @@ mod tests {
     #[derive(Debug, Serialize, Deserialize, PartialEq)]
     struct Document {
         id: usize,
-        value: String,
-        kind: String,
     }
 
     // impl PartialEq<Map<String, Value>> for Document {
@@ -949,31 +950,27 @@ mod tests {
     //     }
     // }
 
-    async fn setup_test_index(client: &Client, index: &Index) -> Result<(), Error> {
-        let t0 = index.add_documents(&[
-            Document { id: 0, kind: "text".into(), value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".to_string() },
-            Document { id: 1, kind: "text".into(), value: "dolor sit amet, consectetur adipiscing elit".to_string() },
-            Document { id: 2, kind: "title".into(), value: "The Social Network".to_string() },
-            Document { id: 3, kind: "title".into(), value: "Harry Potter and the Sorcerer's Stone".to_string() },
-            Document { id: 4, kind: "title".into(), value: "Harry Potter and the Chamber of Secrets".to_string() },
-            Document { id: 5, kind: "title".into(), value: "Harry Potter and the Prisoner of Azkaban".to_string() },
-            Document { id: 6, kind: "title".into(), value: "Harry Potter and the Goblet of Fire".to_string() },
-            Document { id: 7, kind: "title".into(), value: "Harry Potter and the Order of the Phoenix".to_string() },
-            Document { id: 8, kind: "title".into(), value: "Harry Potter and the Half-Blood Prince".to_string() },
-            Document { id: 9, kind: "title".into(), value: "Harry Potter and the Deathly Hallows".to_string() },
-        ], None).await?;
-        let t1 = index.set_filterable_attributes(["kind", "value"]).await?;
-        let t2 = index.set_sortable_attributes(["title"]).await?;
-
-        t2.wait_for_completion(client, None, None).await?;
-        t1.wait_for_completion(client, None, None).await?;
-        t0.wait_for_completion(client, None, None).await?;
-
-        Ok(())
-    }
-
     #[meilisearch_test]
-    async fn test_swapping_two_indexes(client: Client) {}
+    async fn test_swapping_two_indexes(client: Client) {
+        let index_1 = client.index("test_swapping_two_indexes_1");
+        let index_2 = client.index("test_swapping_two_indexes_2");
+
+        let t0 = index_1
+            .add_documents(&[Document { id: 1 }], None)
+            .await
+            .unwrap();
+
+        let t1 = index_2
+            .add_documents(&[Document { id: 2 }], None)
+            .await
+            .unwrap();
+
+        t0.wait_for_completion(&client, None, None).await.unwrap();
+
+        client.swap_indexes([&SwapIndexes {
+            indexes: ["test_swapping_two_indexes_1", "test_swapping_two_indexes_2"],
+        }]);
+    }
 
     #[meilisearch_test]
     async fn test_methods_has_qualified_version_as_header() {
