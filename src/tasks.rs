999ed3480ff071bfc705d8ad6e3ffcd0c5c3a368
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2022-11-21 17:47:41 +0100

    Rename setting detail original filters to original filter for v0.30.0 (#392)

--- src/tasks.rs
+++ src/tasks.rs
@@ -107,7 +107,7 @@ pub struct IndexSwap {
 pub struct TaskCancelation {
     pub matched_tasks: usize,
     pub canceled_tasks: usize,
-    pub original_filters: String,
+    pub original_filter: String,
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -115,7 +115,7 @@ pub struct TaskCancelation {
 pub struct TaskDeletion {
     pub matched_tasks: usize,
     pub deleted_tasks: usize,
-    pub original_filters: String,
+    pub original_filter: String,
 }
 
 #[derive(Deserialize, Debug, Clone)]
