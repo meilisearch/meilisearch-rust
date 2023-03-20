b00db8cd23ab623323b623fee65dee6229ab7513
Charlotte Vermandel <charlottevermandel@gmail.com>
2023-03-20 15:51:02 +0100

    Display whole semver of the SDK in code-samples file

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -4,7 +4,7 @@
 current_tag=$(echo $GITHUB_REF | cut -d '/' -f 3 | sed -r 's/^v//')
 major=$(echo $current_tag | cut -d '.' -f1)
 minor=$(echo $current_tag | cut -d '.' -f2)
-cropped_current_tag="$major.$minor"
+
 file1='Cargo.toml'
 file2='README.tpl'
 file3='.code-samples.meilisearch.yaml'
@@ -21,7 +21,7 @@ file_tag5=$(grep '^version = ' $file5 | grep -Eo '[0-9]+.[0-9]+.[0-9]+')
 if [ "$current_tag" != "$file_tag1" ] ||
   [ "$current_tag" != "$file_tag_1_1" ] ||
   [ "$current_tag" != "$file_tag2" ] ||
-  [ "$cropped_current_tag" != "$file_tag3" ] ||
+  [ "$current_tag" != "$file_tag3" ] ||
   [ "$current_tag" != "$file_tag4" ] ||
   [ "$current_tag" != "$file_tag5" ] \
   ; then
@@ -29,7 +29,7 @@ if [ "$current_tag" != "$file_tag1" ] ||
   echo "$file1: found $file_tag1 - expected $current_tag"
   echo "$file1: found $file_tag_1_1 - expected $current_tag"
   echo "$file2: found $file_tag2 - expected $current_tag"
-  echo "$file3: found $file_tag3 - expected $cropped_current_tag"
+  echo "$file3: found $file_tag3 - expected $current_tag"
   echo "$file4: found $file_tag4 - expected $current_tag"
   echo "$file5: found $file_tag5 - expected $current_tag"
   exit 1
