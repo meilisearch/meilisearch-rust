f909af065ecfb71faefdb86186fb0e71cecbe204
Vladimir Donich <git@funlennysub.dev>
2024-09-01 11:18:07 +0000

    Add support for `#[index_config(index_name = "")]`; Cleanup code

--- meilisearch-index-setting-macro/src/lib.rs
+++ meilisearch-index-setting-macro/src/lib.rs
@@ -1,8 +1,8 @@
 use convert_case::{Case, Casing};
 use proc_macro2::Ident;
 use quote::quote;
-use structmeta::{Flag, StructMeta};
-use syn::{parse_macro_input, spanned::Spanned};
+use structmeta::{Flag, NameValue, StructMeta};
+use syn::{parse_macro_input, spanned::Spanned, Attribute, LitStr};
 
 #[derive(Clone, StructMeta, Default)]
 struct FieldAttrs {
@@ -14,30 +14,51 @@ struct FieldAttrs {
     sortable: Flag,
 }
 
+#[derive(StructMeta)]
+struct StructAttrs {
+    index_name: NameValue<LitStr>,
+}
+
 #[proc_macro_derive(IndexConfig, attributes(index_config))]
 pub fn generate_index_settings(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast = parse_macro_input!(input as syn::DeriveInput);
+    let syn::DeriveInput {
+        attrs, ident, data, ..
+    } = parse_macro_input!(input as syn::DeriveInput);
 
-    let fields: &syn::Fields = match ast.data {
+    let fields: &syn::Fields = match data {
         syn::Data::Struct(ref data) => &data.fields,
         _ => {
             return proc_macro::TokenStream::from(
-                syn::Error::new(ast.ident.span(), "Applicable only to struct").to_compile_error(),
+                syn::Error::new(ident.span(), "Applicable only to struct").to_compile_error(),
             );
         }
     };
 
-    let struct_ident = &ast.ident;
+    let struct_ident = &ident;
 
-    let index_config_implementation = get_index_config_implementation(struct_ident, fields);
+    let index_config_implementation = get_index_config_implementation(struct_ident, fields, attrs);
     proc_macro::TokenStream::from(quote! {
         #index_config_implementation
     })
 }
 
+fn filter_attrs(attrs: &[Attribute]) -> impl Iterator<Item = &Attribute> {
+    attrs
+        .iter()
+        .filter(|attr| attr.path().is_ident("index_config"))
+}
+
+fn get_index_name(struct_ident: &Ident, struct_attrs: &[Attribute]) -> String {
+    filter_attrs(struct_attrs)
+        .find_map(|attr| attr.parse_args::<StructAttrs>().ok())
+        .map(|attr| attr.index_name.value.value())
+        .unwrap_or_else(|| struct_ident.to_string().to_case(Case::Snake))
+}
+
 fn get_index_config_implementation(
     struct_ident: &Ident,
     fields: &syn::Fields,
+    attrs: Vec<Attribute>,
 ) -> proc_macro2::TokenStream {
     let mut primary_key_attribute = String::new();
     let mut distinct_key_attribute = String::new();
@@ -46,23 +67,14 @@ fn get_index_config_implementation(
     let mut filterable_attributes = vec![];
     let mut sortable_attributes = vec![];
 
-    let index_name = struct_ident
-        .to_string()
-        .from_case(Case::UpperCamel)
-        .to_case(Case::Snake);
+    let index_name = get_index_name(struct_ident, &attrs);
 
     let mut primary_key_found = false;
     let mut distinct_found = false;
 
     for field in fields {
-        let attrs = field
-            .attrs
-            .iter()
-            .filter(|attr| attr.path().is_ident("index_config"))
-            .map(|attr| attr.parse_args::<FieldAttrs>().unwrap())
-            .collect::<Vec<_>>()
-            .first()
-            .cloned()
+        let attrs = filter_attrs(&field.attrs)
+            .find_map(|attr| attr.parse_args::<FieldAttrs>().ok())
             .unwrap_or_default();
 
         // Check if the primary key field is unique
@@ -134,20 +146,20 @@ fn get_index_config_implementation(
             const INDEX_STR: &'static str = #index_name;
 
             fn generate_settings() -> ::meilisearch_sdk::settings::Settings {
-            ::meilisearch_sdk::settings::Settings::new()
-            #display_attr_tokens
-            #sortable_attr_tokens
-            #filterable_attr_tokens
-            #searchable_attr_tokens
-            #distinct_attr_token
-        }
+                ::meilisearch_sdk::settings::Settings::new()
+                #display_attr_tokens
+                #sortable_attr_tokens
+                #filterable_attr_tokens
+                #searchable_attr_tokens
+                #distinct_attr_token
+            }
 
-         async fn generate_index<Http: ::meilisearch_sdk::request::HttpClient>(client: &::meilisearch_sdk::client::Client<Http>) -> std::result::Result<::meilisearch_sdk::indexes::Index<Http>, ::meilisearch_sdk::tasks::Task> {
-            return client.create_index(#index_name, #primary_key_token)
-                .await.unwrap()
-                .wait_for_completion(&client, ::std::option::Option::None, ::std::option::Option::None)
-                .await.unwrap()
-                .try_make_index(&client);
+            async fn generate_index<Http: ::meilisearch_sdk::request::HttpClient>(client: &::meilisearch_sdk::client::Client<Http>) -> std::result::Result<::meilisearch_sdk::indexes::Index<Http>, ::meilisearch_sdk::tasks::Task> {
+                client.create_index(#index_name, #primary_key_token)
+                    .await.unwrap()
+                    .wait_for_completion(client, ::std::option::Option::None, ::std::option::Option::None)
+                    .await.unwrap()
+                    .try_make_index(client)
             }
         }
     }
