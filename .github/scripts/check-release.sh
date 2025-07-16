83cdb4ae3786b8df45c4a792cb5cd2ba3dbc77fe
Mubelotix <simon@meilisearch.com>
2025-07-16 12:11:01 +0200

    Revert "Merge #685"
    This reverts commit fbb7807360f031fc04e50d8f9db2ea75725f0ed4, reversing
    changes made to f9f4e53d4f3fcb4e8af27704936f6588f802e42e.

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -10,12 +10,14 @@ file4='README.md'
 file5='./meilisearch-index-setting-macro/Cargo.toml'
 
 file_tag1=$(grep '^version = ' $file1 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
+file_tag_1_1=$(grep '{ path = "meilisearch-index-setting-macro", version =' $file1 | grep -Eo '[0-9]+.[0-9]+.[0-9]+')
 file_tag2=$(grep 'meilisearch-sdk = ' $file2 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 file_tag3=$(grep 'meilisearch-sdk = ' $file3 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 file_tag4=$(grep 'meilisearch-sdk = ' $file4 | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 file_tag5=$(grep '^version = ' $file5 | grep -Eo '[0-9]+.[0-9]+.[0-9]+')
 
 if [ "$current_tag" != "$file_tag1" ] ||
+  [ "$current_tag" != "$file_tag_1_1" ] ||
   [ "$current_tag" != "$file_tag2" ] ||
   [ "$current_tag" != "$file_tag3" ] ||
   [ "$current_tag" != "$file_tag4" ] ||
@@ -23,6 +25,7 @@ if [ "$current_tag" != "$file_tag1" ] ||
   ; then
   echo "Error: the current tag does not match the version in package file(s)."
   echo "$file1: found $file_tag1 - expected $current_tag"
+  echo "$file1: found $file_tag_1_1 - expected $current_tag"
   echo "$file2: found $file_tag2 - expected $current_tag"
   echo "$file3: found $file_tag3 - expected $current_tag"
   echo "$file4: found $file_tag4 - expected $current_tag"
--- scripts/update_macro_versions.sh
+++ scripts/update_macro_versions.sh
@@ -7,3 +7,4 @@ old_index_macro_version=$(grep '^version = ' ./meilisearch-index-setting-macro/C
 old_macro_in_sdk_version=$(grep '{ path = "meilisearch-index-setting-macro", version =' ./Cargo.toml)
 
 sed -i '' -e "s/^$old_index_macro_version/$new_version/g" './meilisearch-index-setting-macro/Cargo.toml'
+sed -i '' -e "s/$old_macro_in_sdk_version/meilisearch-index-setting-macro = { path = \"meilisearch-index-setting-macro\", $new_version }/g" './Cargo.toml'
